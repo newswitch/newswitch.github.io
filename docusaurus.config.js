92b81f517582e3d77bfa88755fcdfeaf7fb35f12
newswitch <38205603+newswitch@users.noreply.github.com>
2026-08-31 10:25:06 +0800

    config: show all blog posts in sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,6 +46,7 @@ const config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarCount: 'ALL',
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
